349c89b9e2e1dbe54c8208b718f5b80d8eb085a2
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2025-07-12 02:15:29 +0700

    feat : change descriptions

--- src/pages/ProjectsPage/components/TitleSection/TitleSection.tsx
+++ src/pages/ProjectsPage/components/TitleSection/TitleSection.tsx
@@ -6,9 +6,7 @@ const TitleSection = () => {
           Project <span className='text-primary'>Collections</span>
         </h2>
         <p className='text-base md:text-lg mt-4 text-center text-neutral-content/80 mb-8 mx-24'>
-          Explore all of my projects, including web, mobile, and backend solutions. Use the filters
-          to find projects by technology, platform, or year. For a quick overview, try searching by
-          keyword.
+          Every project here was created to solve problems I faced at work or in daily life.
         </p>
       </div>
     </>
