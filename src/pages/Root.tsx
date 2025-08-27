a0b24dc2493518279f9877f8432e71f963d97b21
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2025-08-27 20:34:50 +0700

    Feat feature/seo-optimization: Remove unnecessary meta tag controller

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '@/components/Navbar/Navbar'
 import Footer from '@/components/Footer/Footer'
-import MetaTagController from '@/components/MetaTagController/MetaTagController'
 
 const Root = () => {
   return (
     <div className='min-h-screen flex flex-col justify-center items-center relative'>
-      <MetaTagController />
       <Navbar />
       <Outlet />
       <Footer />
