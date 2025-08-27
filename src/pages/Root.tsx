907d5c4ef64287b7303b824efeabd1ba2dd18d24
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2025-08-27 21:09:06 +0700

    Feat feature/seo-optimization: Add several adjustment for index.html

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '@/components/Navbar/Navbar'
 import Footer from '@/components/Footer/Footer'
+import MetaTagController from '@/components/MetaTagController/MetaTagController'
 
 const Root = () => {
   return (
     <div className='min-h-screen flex flex-col justify-center items-center relative'>
+      <MetaTagController />
       <Navbar />
       <Outlet />
       <Footer />
